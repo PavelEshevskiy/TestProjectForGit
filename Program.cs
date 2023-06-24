9d2bd85a59daa8572295673cef74d153cad25413
PavelEshevskiy <Pashkas123@yandex.ru>
2023-06-25 01:06:24 +0300

    disabled Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
-﻿using System.Linq;
+﻿// using System.Linq;
 
-Console.Write("Input data separated by ', ':");
-string? str = Console.ReadLine();
-string [] array = str.Split(", ")
-                     .ToArray();
+// Console.Write("Input data separated by ', ':");
+// string? str = Console.ReadLine();
+// string [] array = str.Split(", ")
+//                      .ToArray();
 
 
 // void ShowArray (string[] array)
